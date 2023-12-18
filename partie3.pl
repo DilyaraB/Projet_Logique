0c83031c58a69d6f8006fd4a389bf83248e54fd1
salwamujahid <salwamujahid.mlb@gmail.com>
2023-12-18 10:07:24 +0100

    Affichage + Affichage de règles

--- partie3.pl
+++ partie3.pl
@@ -1,28 +1,4 @@
-/* Règles :
-    - Existe
-    - ET
-    - Pour tout
-    - OU
-
-*/
-
 /*
-
-TBox
-
-[(sculpteur,and(personne,some(aCree,sculpture))),
-(auteur,and(personne,some(aEcrit,livre))),
-(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))),
-(parent,and(personne,some(aEnfant,anything)))]
-
-ABox
-
-[(michelAnge,personne), (david,sculpture), (sonnets,livre),(vinci,personne), (joconde,objet)]
-[(michelAnge,and(auteur,some(aEcrit,livre)), (david,some(aEnfant,anything))]
-
-Roles
-
-[(michelAnge, david, aCree), (michelAnge, sonnet, aEcrit),(vinci,joconde, aCree)]
 
 Noms 
 
@@ -35,14 +11,17 @@ Ls : Liste ?
 */
 
 compteur(1).
+
 troisieme_etape(Abi,Abr) :-
     tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
-    affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls, Lie, Lpt, Li, Lu, Abr),
+    affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, [], [], [], [], [], []),
     resolution(Lie,Lpt,Li,Lu,Ls,Abr),
     nl,
     write('Youpiiiiii, on a demontre la proposition initiale !!!'),!.
 
-/* tri_Abox */
+/*  ----------------------
+         tri_Abox 
+    ---------------------- */
 
 tri_Abox([], [], [], [], [], []).
 
@@ -59,14 +38,15 @@ tri_Abox([(I, or(C1, C2))|L], Lie, Lpt, Li, [(I, or(C1, C2))|Lu], Ls) :-
     tri_Abox(L, Lie, Lpt, Li, Lu, Ls),!.
 
 tri_Abox([(I, C)|L], Lie, Lpt, Li, Lu, [(I, C)|Ls]) :-
-    setof(X, cnamea(X), Lca),
-    member(C, Lca),
+    cnamea(C),
     tri_Abox(L, Lie, Lpt, Li, Lu, Ls),!.
 
 tri_Abox([(I, not(C))|L], Lie, Lpt, Li, Lu, [(I, not(C))|Ls]) :-
     tri_Abox(L, Lie, Lpt, Li, Lu, Ls),!.
 
-/* resolution */
+/*  ----------------------
+         Résolution
+    ---------------------- */
 
 resolution([],[],[],[],[],[]).
 
@@ -75,19 +55,18 @@ resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
 
 /* 1. clash ? oui ! */
 is_clash(Lie, Lpt, Li, Lu, Ls, Abr) :-
-    setof(X, rname(X), Lr),
-    member(R, Lr),
+    rname(R),
     evolue(Abr, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
     member((I1, I2, R), Ls1),
     member((I1, I2, not(R)), Ls1),
-    affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr).
+    affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr2).
 
 /* 2. clash ? oui ! */
 is_clash(Lie, Lpt, Li, Lu, Ls, Abr) :-
     evolue(Abr, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),
     member((I, C), Ls1),
     member((I, not(C)), Ls1),
-    affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr).
+    affiche_evolution_Abox(Ls, Lie, Lpt, Li, Lu, Abr, Ls1, Lie1, Lpt1, Li1, Lu1, Abr2).
 
 /* 3. clash ? non ! */
 is_clash(Lie,Lpt,Li,Lu,Ls,Abr) :-
@@ -101,8 +80,8 @@ complete_some([],Lpt,Li,Lu,Ls,[]) :-
 
 /* 5. ∃ fait */
 complete_some([(I, some(R, C))|Lie],Lpt,Li,Lu,Ls,Abr) :-
-    setof(X, iname(X), Linst),
-    member(I2, Linst),
+    write("Regle \u2203"), nl,
+    iname(I2),
     concat([(I, I2, R), (I2, C)], Abr, Res),
     is_clash(Lie,Lpt,Li,Lu,Ls,Res).
 
@@ -112,6 +91,7 @@ transformation_and(Lie,Lpt,[],Lu,Ls,[]) :-
 
 /* 7. ⊓ fait */
 transformation_and(Lie,Lpt,[(I, and(C1, C2))|Li],Lu,Ls,Abr) :-
+    write("Regle \u2A05"), nl,
     concat([(I, C1), (I, C2)], Abr, Res),
     is_clash(Lie,Lpt,Li,Lu,Ls,Res).
 
@@ -121,6 +101,7 @@ deduction_all(Lie,[],Li,Lu,Ls,[]) :-
 
 /* 9. ∀ fait */
 deduction_all(Lie,[(I, all(R, C))|Lpt],Li,Lu,Ls,Abr) :-
+    write("Regle \u2200"), nl,
     concat([(I, C)], Abr, Res),
     is_clash(Lie,Lpt,Li,Lu,Ls,Res).
 
@@ -128,15 +109,19 @@ deduction_all(Lie,[(I, all(R, C))|Lpt],Li,Lu,Ls,Abr) :-
 
 /* 11.1. ⊔ fait */
 transformation_or(Lie,Lpt,Li,[(I, or(C1, C2))|Lu],Ls,Abr) :-
+    write("Regle \u2A06 (branche 1)"), nl,
     concat([(I, C1)], Abr, Res1),
     is_clash(Lie,Lpt,Li,Lu,Ls,Res1).
 
 /* 11.2. ⊔ fait */
 transformation_or(Lie,Lpt,Li,[(I, or(C1, C2))|Lu],Ls,Abr) :-
+    write("Regle \u2A06 (branche 2)"), nl,
     concat([(I, C2)], Abr, Res2),
     is_clash(Lie,Lpt,Li,Lu,Ls,Res2).
 
-/* evolue */
+/*  ----------------------
+         Evolue
+    ---------------------- */
 
 evolue([], Lie, Lpt, Li, Lu, Ls, Lie, Lpt, Li, Lu, Ls).
 
@@ -153,14 +138,15 @@ evolue([(I, or(C1, C2))|L], Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, [(I, or(C1, C
     evolue(L, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),!.
 
 evolue([(I, C)|L], Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, [(I, C)|Ls1]) :-
-    setof(X, cnamea(X), Lca),
-    member(C, Lca),
+    cnamea(C),
     evolue(L, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),!.
 
 evolue([(I, not(C))|L], Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, [(I, not(C))|Ls1]) :-
     evolue(L, Lie, Lpt, Li, Lu, Ls, Lie1, Lpt1, Li1, Lu1, Ls1),!.
 
-/* affiche_evolution_Abox */
+/*  ---------------------------
+         Affiche évolution
+    --------------------------- */
 
 affiche_concept(some(R, C)) :-
     write("\u2203"), 
@@ -188,44 +174,27 @@ affiche_concept(not(C)) :-
     write(")").
 
 affiche_concept(C) :-
+    cnamea(C),
     write(C).
 
 affiche_assertion([]).
 
 affiche_assertion([(I, C)|L]):-
-	write(I), write(' : '), affiche_concept(C), nl,
+	write("| "), write(I), write(' : '), affiche_concept(C), nl,
 	affiche_assertion(L).
 
-/*
-Qu'est-ce qu'on fait de Abr ?
-*/
-
 affiche_evolution_Abox(Ls1, Lie1, Lpt1, Li1, Lu1, Abr1, Ls2, Lie2, Lpt2, Li2, Lu2, Abr2) :-
-    write("_____________________"), nl,
+    write(" ________________________________________"), nl,
+    write("/                                        \\"), nl,
     affiche_assertion(Ls1),
     affiche_assertion(Lie1),
     affiche_assertion(Lpt1),
     affiche_assertion(Li1),
     affiche_assertion(Lu1),
-    write(Abr1), nl,
-
-    write("-------------"), nl,
+    write("|----------------------------------------"), nl,
     affiche_assertion(Ls2),
     affiche_assertion(Lie2),
     affiche_assertion(Lpt2),
     affiche_assertion(Li2),
     affiche_assertion(Lu2),
-    write(Abr2), nl,
-    
-    write("_____________________"), nl.
-
-
-/*
-Annexe
-
-member(X,L)
-concat(L1,L2,L3)
-enleve(X,L1,L2)
-genere(Nom)
-flatten(Liste1,Liste2)
-*/
+    write("\\________________________________________/"), nl.
